6dc38fe258c610daa759b90bb5ea01adb30a9e5b
Bohdan Pylypchenko <bogdan314ylypchenko@gmail.com>
2022-05-16 18:53:16 +0300

    Adding MonteCarlo PI implementation

---
+++ mc_pi/mc_pi/mc_pi.h
@@ -0,0 +1,10 @@
+#ifndef MONTECARLO_PI_DEMO_MC_PI_H
+#define MONTECARLO_PI_DEMO_MC_PI_H
+
+/*
+ * Returns pi constant, calculated by Monte Carlo method
+ * Takes vector count to generate
+ */
+long double mc_pi(const int v_count);
+
+#endif
